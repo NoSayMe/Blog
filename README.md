c6da39f3676eb625b8184292db168f0a67a9ab1b
NoSayMe <gabriel.juraj@gmail.com>
2019-01-20 12:08:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Features
+========
+1. Ready-to-go django blog
+
+
+Environment setup
+=================
+1. Install pipenv
+
+
+New project kick-off
+====================
+How to fork it:
+1. Go to https://codecloud.web.att.com/projects/ST_BRATISLAVA/repos/template_django?fork
+
+
+Local development
+=================
+1. Set up DB connection:
+    ```
+    [DB_DEV]
+    ```
+
+1. Set up proxy: put the following content to `~/.docker/config.json`
+   
+    ```json
+    text
+    ```
+   
---
+++ blog/posts/views.py
@@ -0,0 +1,27 @@
+import os
+from .models import Post
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.order_by('-pub_date')
+    return render(request, 'posts/home.html', {'posts':posts})
+
+def post_details(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/posts_detail.html', {'post':post})
+
+def cv(request):
+    return render(request, 'posts/cv.html')
+
+def download(request):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'posts/static/juraj_gabriel_cv.pdf')
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type='application/pdf')
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
