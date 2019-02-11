ffb70515a96662ca741764de7670f85c6881f168
NoSayMe <gabriel.juraj@gmail.com>
2019-02-11 17:55:24 +0100

    [update] sitepages.views

--- blog/sitepages/views.py
+++ blog/sitepages/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from .forms import ContactForm
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import send_mail
 from django.shortcuts import redirect
-from django.template.loader import get_template
+from django.contrib import messages
 
 
-# Create your views here.
 def about(request):
     return render(request, 'sitepages/about.html')
 
@@ -15,23 +14,10 @@ def contact(request):
     if request.method == 'POST':
         form = form_class(data=request.POST)
         if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-                , '')
-            contact_email = request.POST.get(
-                'contact_email'
-                , '')
-            form_content = request.POST.get(
-                'content'
-                , '')
-            template = get_template('sitepages/contact_template.txt')
-            context = {
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-            }
-            content = template.render(context)
-            send_mail('Blog contact', form_content, contact_email, ['gabriel.juraj@gmail.com'], fail_silently=False)
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            form_content = request.POST.get('content', '')
+            send_mail('Blog contact' + contact_name, form_content, contact_email, ['gabriel.juraj@gmail.com'], fail_silently=False)
             return redirect('contact')
     return render(request, 'sitepages/contact.html', {
         'form': form_class,
